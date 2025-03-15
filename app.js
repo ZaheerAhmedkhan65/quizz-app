7ec27f28c4b2629e8af079d361576414bbcf8293
Zaheer Ahmed <ranazaheerahmed65@gmail.com>
2025-03-16 00:14:42 +0500

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(express.urlencoded({ extended: true }));
 
-// app.set('public', path.join(__dirname, 'public'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
