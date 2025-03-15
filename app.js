92538524554e6d3853e4af132a13c9c5347f8634
Zaheer Ahmed <ranazaheerahmed65@gmail.com>
2025-03-15 22:23:37 +0500

    new commit

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const authRoutes = require('./routes/authRoutes');
@@ -16,7 +19,7 @@ const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const quizzRoutes = require('./routes/quizzRoutes');
 const authenticate = require('./middleware/authenticate');
-let user = null;
+
 app.use("/auth",authRoutes);
 app.use("/api",userRoutes);
 app.use("/api/courses",courseRoutes);
