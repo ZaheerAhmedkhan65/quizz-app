9eeb6f1d17faf32a8e10958903d6c67ed7fdf959
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2025-10-29 02:23:08 +0500

    minor fixes in auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -59,7 +59,7 @@ router.get(
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
   
